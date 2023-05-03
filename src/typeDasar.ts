2c398f667de789fc740d56019e9c5da1ce6122ce
noviamarsheila <noviasheila123@gmail.com>
2023-05-03 18:39:34 +0700

    number and boolean

--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -1,3 +1,11 @@
-let nama = "Novia Marsheila Louisyana";
-
+// string
+let nama: string = "Novia Marsheila Louisyana";
 nama = "sheila";
+
+// number
+let umur: number;
+umur = 20;
+
+// boolean
+let isMarried: boolean;
+isMarried = true;
