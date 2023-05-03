5c7cb71a3d1a286db49ac0eb32629a9a783d4c2d
noviamarsheila <noviasheila123@gmail.com>
2023-05-03 19:58:17 +0700

    union

--- build/typeDasar.js
+++ build/typeDasar.js
@@ -14,3 +14,9 @@ heroes = 20;
 heroes = [];
 heroes = {};
 heroes = true;
+// Union type
+6282356;
+("0823784");
+let phone;
+phone = "08237462727";
+phone = 08432989043;
--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -16,3 +16,11 @@ heroes = 20;
 heroes = [];
 heroes = {};
 heroes = true;
+
+// Union type
+6282356;
+("0823784");
+
+let phone: number | string;
+phone = "08237462727";
+phone = 08432989043;
