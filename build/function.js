4aa69e7da87afdaf6464116bd0b79e7c59d87f35
noviamarsheila <noviasheila123@gmail.com>
2023-05-03 19:09:46 +0700

    function

---
+++ build/function.js
@@ -0,0 +1,12 @@
+"use strict";
+// tipe data pada balikan function
+function getName() {
+    return "Hello, My name is sheila";
+}
+console.log(getName());
+function getAge() {
+    return 20;
+}
+function printName() {
+    console.log("Sheila");
+}
---
+++ src/function.ts
@@ -0,0 +1,15 @@
+// tipe data pada balikan function
+
+function getName(): string {
+	return "Hello, My name is sheila";
+}
+
+console.log(getName());
+
+function getAge(): number {
+	return 20;
+}
+
+function printName(): void {
+	console.log("Sheila");
+}
